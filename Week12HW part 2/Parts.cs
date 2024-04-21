c5be19f14ce82f5841476fcce20573004caca528
Derek <Drj2778@email.vccs.edu>
2024-04-21 14:59:38 -0400

    Add project files.

---
+++ Week12HW part 2/Parts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week12HW_part_2
+{
+    public class Parts
+    {
+        int partNumber;
+        string pName, pDescription;
+        double cost;
+
+
+        public Parts(int pNumb, string pn, string pd, double c) 
+        { 
+            
+            partNumber = pNumb;
+            pName = pn;
+            pDescription = pd;
+            Cost = c;
+                 
+        }
+                  
+        public int PartNumber 
+        { 
+         get { return partNumber; }
+            set { partNumber = value; }
+        
+        
+        }
+        public string PName 
+        { 
+         get { return pName; }
+            set { pName = value; }
+        
+        
+        }
+
+        public string PDescription
+        { 
+            
+            get { return pDescription; }         
+             set { pDescription = value; }
+        }
+
+        public double Cost 
+        {
+            get { return cost;}
+            set { cost = value; }
+        
+        
+        }
+    }
+
+    
+}
---
+++ Week12HW part 2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using static System.Console;
+
+namespace Week12HW_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numParts;
+
+            WriteLine("How many parts do you wish to enter?");
+
+            numParts = Convert.ToInt32(Console.ReadLine());
+
+            Parts[] partArray = new Parts[numParts];
+
+            for (int x = 0; x < partArray.Length; x++)
+            {
+                populatePart(ref partArray[x]);
+
+            }
+
+
+            static void populatePart(ref Parts e)
+            {
+                WriteLine("Please enter part number");
+                int pNumb = Convert.ToInt32(Console.ReadLine());
+
+                WriteLine("Please enter part name");
+                string pn = ReadLine();
+
+                WriteLine("Please enter part description");
+                string pd = ReadLine();
+
+                WriteLine("Please enter part cost");
+                double c = Convert.ToDouble(Console.ReadLine());
+
+
+                e = new Parts(pNumb, pn, pd, c);
+
+            }
+
+            WriteLine("Which part would you like to view the data on?");
+
+            for (int z = 0; z < partArray.Length; z++)
+            {
+                WriteLine(z + 1 + " " + partArray[z].PName);
+
+
+            }
+
+            int selectedPosition = Convert.ToInt32(Console.ReadLine());
+
+            Parts selectedPart = partArray[selectedPosition - 1];
+
+
+            WriteLine("{0,20},{1,20},{2,20},{3,20 }", "Part Number", "Part Name" ,"Part Description", "Part Cost");
+
+           
+
+            WriteLine("{0,20},{1,20},{2,20},{3,20}", selectedPart.PartNumber, selectedPart.PName, selectedPart.PDescription, selectedPart.Cost);
+
+
+
+
+
+
+
+        }
+    }
+}
